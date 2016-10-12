f3b9c57838749a80005835c25467f346747e48c8
Seth Fitzsimmons <seth@mojodna.net>
2016-10-11 17:44:59 -0700

    Lambda-powered tiling

--- README.md
+++ README.md
@@ -65,3 +65,67 @@ aws s3 cp \
   scene-0-image-0-DG-103001005E85AC00.vrt \
   s3://oam-dynamic-tiler-tmp/uploads/2016-10-11/57fca69e84ae75bb00ec751f/scene/0/
 ```
+
+## lambda
+
+Create IAM role: `tiler_lambda_function` with Trust Relationship policy document:
+
+```xml
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+```
+
+Create IAM policy: `tiler_lambda_logs` with policy document:
+
+```xml
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "logs:*"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        }
+    ]
+}
+```
+
+Attach policy to `tiler_lambda_function` role.
+
+Create inline policy to allow `tiler_lambda_function` to write to S3:
+
+```xml
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:PutObjectAcl"
+            ],
+            "Resource": [
+                "arn:aws:s3:::oam-dynamic-tiler-tmp/*"
+            ]
+        }
+    ]
+}
+```
+
+Update `project.json` with `tiler_lambda_function`'s Role ARN, e.g. `arn:aws:iam::670261699094:role/tiler_lambda_function`.
+
+Run `apex deploy`. (Add `-l debug` to see what's running.) This will build the Docker image defined
+in `deps/` to produce a `task.zip` containing binary dependencies needed when deploying to the
+Lambda runtime.
---
+++ functions/tiler/main.py
@@ -0,0 +1,82 @@
+# coding=utf-8
+
+import os
+from os import path
+import json
+import re
+
+import boto3
+from mercantile import Tile
+from raven import Client
+
+from tiler import InvalidTileRequest, read_tile
+
+
+sentry = Client()
+S3_BUCKET = os.environ["S3_BUCKET"]
+S3 = boto3.resource("s3")
+FORMAT = os.environ.get("FORMAT", "png")
+
+
+def handle(event, context):
+    (input_path, ext) = path.splitext(event["path"])
+    parts = input_path.split("@")
+    input_path = parts.pop(0)
+
+    (y, x, zoom, scene, _) = reversed(input_path.split("/"))
+    y = int(y)
+    x = int(x)
+    zoom = int(zoom)
+
+    if len(parts) > 0:
+        scale = int(re.sub(r"[^\d]", "", parts.pop()))
+    else:
+        scale = 1
+
+    format = ext[1:]
+
+    if format != FORMAT:
+        raise InvalidTileRequest("Invalid format")
+
+    try:
+        tile = Tile(x, y, zoom)
+        data = read_tile(id, tile, scale=scale)
+
+        if scale == 1:
+            key = "{}/{}/{}/{}.{}".format(scene, tile.z, tile.x, tile.y, FORMAT)
+
+            S3.Object(
+                S3_BUCKET,
+                key,
+            ).put(
+                Body=data,
+                ACL="public-read",
+                ContentType="image/{}".format(FORMAT),
+                CacheControl="public, max-age=2592000",
+                StorageClass="REDUCED_REDUNDANCY",
+            )
+        elif scale == 2:
+            key = "{}/{}/{}/{}@2x.{}".format(scene, tile.z, tile.x, tile.y, FORMAT)
+
+            S3.Object(
+                S3_BUCKET,
+                key,
+            ).put(
+                Body=data,
+                ACL="public-read",
+                ContentType="image/{}".format(FORMAT),
+                CacheControl="public, max-age=2592000",
+                StorageClass="REDUCED_REDUNDANCY",
+            )
+    except InvalidTileRequest:
+        raise
+    except:
+        sentry.captureException()
+        raise
+
+    return {
+        "statusCode": 302,
+        "headers": {
+            "Location": "http://{}.s3.amazonaws.com/{}".format(S3_BUCKET, key),
+        }
+    }
