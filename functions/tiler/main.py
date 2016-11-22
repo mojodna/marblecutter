e85a99ba496f388119683574769f1563d2b6bfde
Seth Fitzsimmons <seth@mojodna.net>
2016-11-22 12:16:38 -0800

    WMTS support (Lambda)
    Also refactors routing code to use werkzeug (much cleaner!).

--- functions/tiler/main.py
+++ functions/tiler/main.py
@@ -6,45 +6,30 @@ import json
 import re
 
 import boto3
+from jinja2 import Template
 from mercantile import Tile
 from raven import Client
+from werkzeug.routing import Map, NotFound, Rule
 
-from tiler import InvalidTileRequest, read_tile
+from tiler import InvalidTileRequest, get_metadata, read_tile
 
 
 sentry = Client()
 S3_BUCKET = os.environ["S3_BUCKET"]
 S3 = boto3.resource("s3")
 FORMAT = os.environ.get("FORMAT", "png")
+WMTS_TEMPLATE = Template(open("templates/wmts.xml", "r").read())
 
 
-def handle(event, context):
+routes = Map([
+    Rule("/<scene>/wmts", endpoint="wmts"),
+    Rule("/<scene>/<int:zoom>/<int:x>/<int:y>.{}".format(FORMAT), endpoint="tile"),
+    Rule("/<scene>/<int:zoom>/<int:x>/<int:y>@<int:scale>x.{}".format(FORMAT), endpoint="tile"),
+])
+
+
+def tile(scene, zoom, x, y, scale=1, **kwargs):
     try:
-        (input_path, ext) = path.splitext(event["path"])
-        parts = input_path.split("@")
-        input_path = parts.pop(0)
-
-        (y, x, zoom, scene, _) = reversed(input_path.split("/"))
-        y = int(y)
-        x = int(x)
-        zoom = int(zoom)
-
-        if len(parts) > 0:
-            scale = int(re.sub(r"[^\d]", "", parts.pop()))
-        else:
-            scale = 1
-
-        format = ext[1:]
-    except:
-        # fail gracefully when a path doesn't match
-        return {
-            "statusCode": 404,
-        }
-
-    try:
-        if format != FORMAT:
-            raise InvalidTileRequest("Invalid format")
-
         tile = Tile(x, y, zoom)
         data = read_tile(scene, tile, scale=scale)
 
@@ -82,9 +67,6 @@ def handle(event, context):
             },
             "body": json.dumps(error.to_dict()),
         }
-    except:
-        sentry.captureException()
-        raise
 
     return {
         "statusCode": 302,
@@ -92,3 +74,60 @@ def handle(event, context):
             "Location": "http://{}.s3.amazonaws.com/{}".format(S3_BUCKET, key),
         }
     }
+
+
+def wmts(scene, **kwargs):
+    data = WMTS_TEMPLATE.render(
+        base_url="http://{}.s3-website-us-east-1.amazonaws.com/{}".format(S3_BUCKET, scene),
+        bounds=get_metadata(scene)["bounds"],
+        id=scene,
+    )
+
+    S3.Object(
+        S3_BUCKET,
+        "{}/wmts".format(scene),
+    ).put(
+        Body=data,
+        ACL="public-read",
+        ContentType="application/xml",
+        CacheControl="public, max-age=2592000",
+        StorageClass="REDUCED_REDUNDANCY",
+    )
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/xml",
+        },
+        "body": data,
+    }
+
+
+def handle(event, context):
+    try:
+        (endpoint, args) = routes.bind("", path_info=event["path"]).match()
+        scale = args.get('scale', 1)
+    except NotFound:
+        return {
+            "statusCode": 404,
+        }
+
+    try:
+        return globals()[endpoint](**args)
+    except:
+        sentry.captureException()
+        raise
+
+
+if __name__ == '__main__':
+    print(handle({
+        'path': '/57fc935b84ae75bb00ec751b/wmts',
+    }, None))
+
+    print(handle({
+        'path': '/57fc935b84ae75bb00ec751b/12/1202/1833.png',
+    }, None))
+
+    print(handle({
+        'path': '/57fc935b84ae75bb00ec751b/12/1202/1833@2px.png',
+    }, None))
