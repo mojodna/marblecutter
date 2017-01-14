e190e29b0c1b582f014033584d666f9f7b29e365
Seth Fitzsimmons <seth@mojodna.net>
2017-01-14 09:37:58 -0800

    Scene construction

---
+++ bin/make_scenes.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+bucket="oin-hotosm"
+
+for upload in $(aws s3 ls s3://${bucket}/ | awk '{print $2}'); do
+  echo "Upload ${upload}"
+  for scene in $(aws s3 ls s3://${bucket}/$upload | awk '{print $2}'); do
+    echo "Scene ${scene}"
+    aws s3 ls s3://${bucket}/${upload}${scene} | \
+      grep json | \
+      grep -v footprint | \
+      grep -v scene | \
+      awk "{print \"http://${bucket}.s3.amazonaws.com/${upload}${scene}\" \$4 }" | \
+      xargs bin/make_a_scene.js | \
+      aws s3 cp - s3://oin-hotosm/${upload}${scene}scene.json --acl public-read
+  done
+done
