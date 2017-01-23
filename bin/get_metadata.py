7877d4f46aa678aaaa00ddc2f303708eb451c125
Seth Fitzsimmons <seth@mojodna.net>
2017-01-23 10:51:38 -0800

    Populate tiler metadata w/ selected OIN metadata
    Fixes #7

--- bin/get_metadata.py
+++ bin/get_metadata.py
@@ -9,6 +9,7 @@ import os
 import re
 import sys
 
+import arrow
 import click
 import rasterio
 from rasterio.warp import transform_bounds
@@ -16,10 +17,29 @@ from rasterio.warp import transform_bounds
 from get_zoom import get_zoom, get_zoom_offset
 
 
-@click.command()
+@click.command(context_settings={
+    'ignore_unknown_options': True,
+})
 @click.option("--include-mask", is_flag=True, help="Include a mask URL")
+@click.option("-t", "--title", help="Source title")
+@click.option("-a", "--acquisition-start", help="Acquisition start date")
+@click.option("-A", "--acquisition-end", help="Acquisition end date")
+@click.option("-p", "--provider", help="Provider / owner")
+@click.option("-P", "--platform", help="Imagery platform")
+@click.option("-U", "--uploaded-at", help="Uploaded at")
+@click.argument("_", nargs=-1, type=click.UNPROCESSED)
 @click.argument("prefix")
-def get_metadata(include_mask, prefix):
+def get_metadata(
+    include_mask,
+    title,
+    acquisition_start,
+    acquisition_end,
+    provider,
+    platform,
+    uploaded_at,
+    prefix,
+    _,
+):
     scene = "{}.tif".format(prefix)
     scene_vrt = "{}_warped.vrt".format(prefix)
     mask_vrt = "{}_warped_mask.vrt".format(prefix)
@@ -49,13 +69,27 @@ def get_metadata(include_mask, prefix):
                     "width": src.width,
                   },
                   "minzoom": minzoom,
-                  # TODO provide a name
-                  "name": prefix,
+                  "name": title or source,
                   "tilejson": "2.1.0"
                 }
 
                 if include_mask:
-                    meta['meta']['mask'] = mask
+                    meta["meta"]["mask"] = mask
+
+                if acquisition_start:
+                    meta["meta"]["acquisitionStart"] = arrow.get(acquisition_start).for_json()
+
+                if acquisition_end:
+                    meta["meta"]["acquisitionEnd"] = arrow.get(acquisition_end).for_json()
+
+                if platform:
+                    meta["meta"]["platform"] = platform
+
+                if provider:
+                    meta["meta"]["provider"] = provider
+
+                if uploaded_at:
+                    meta["meta"]["uploadedAt"] = arrow.get(uploaded_at).for_json()
 
                 print(json.dumps(meta))
         except (IOError, rasterio._err.CPLE_HttpResponseError) as e:
