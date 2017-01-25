eab9bd7584f86ff0b80f15a58b1d2d2599346bd5
Seth Fitzsimmons <seth@mojodna.net>
2017-01-25 12:37:51 -0800

    Proper WSGI proxying

---
+++ awsgi.py
@@ -0,0 +1,105 @@
+# https://github.com/slank/awsgi
+#
+# MIT License
+#
+# Copyright (c) 2016 Matthew Wedgwood
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import base64
+from io import BytesIO
+import sys
+try:
+    # Python 3
+    from urllib.parse import urlencode
+except:
+    # Python 2
+    from urllib import urlencode
+
+
+def response(app, event, context):
+    sr = StartResponse()
+    output = app(environ(event, context), sr)
+    return sr.response(output)
+
+
+class StartResponse:
+    def __init__(self):
+        self.status = 500
+        self.headers = []
+        self.body = BytesIO()
+
+    def __call__(self, status, headers, exc_info=None):
+        self.status = status.split()[0]
+        self.headers[:] = headers
+        return self.body.write
+
+    def response(self, output):
+        resp = {
+            'statusCode': str(self.status),
+            'headers': dict(self.headers),
+            'body': self.body.getvalue(),
+        }
+
+        if output:
+            body = bytearray(resp['body'])
+            [body.extend(chunk) for chunk in output]
+
+            resp['body'] = base64.b64encode(body)
+            resp['isBase64Encoded'] = True
+
+        return resp
+
+
+def environ(event, context):
+    if event.get('isBase64Encoded', False) and event.has_key('body'):
+        event['body'] = base64.b64decode(event['body'])
+
+    environ = {
+        'REQUEST_METHOD': event['httpMethod'],
+        'SCRIPT_NAME': '',
+        'PATH_INFO': event['path'],
+        'QUERY_STRING': urlencode(event['queryStringParameters'] or {}),
+        'REMOTE_ADDR': '127.0.0.1',
+        'CONTENT_LENGTH': str(len(event.get('body', '') or '')),
+        'HTTP': 'on',
+        'SERVER_PROTOCOL': 'HTTP/1.1',
+        'wsgi.version': (1, 0),
+        'wsgi.input': BytesIO(event.get('body')),
+        'wsgi.errors': sys.stderr,
+        'wsgi.multithread': False,
+        'wsgi.multiprocess': False,
+        'wsgi.run_once': False,
+    }
+    headers = event.get('headers') or {}
+    for k, v in headers.items():
+        k = k.title()
+        if k == 'Content-Type':
+            environ['CONTENT_TYPE'] = v
+        if k == 'Host':
+            environ['SERVER_NAME'] = v
+        if k == 'X-Forwarded-For':
+            environ['REMOTE_ADDR'] = v.split(', ')[0]
+        if k == 'X-Forwarded-Proto':
+            environ['wsgi.url_scheme'] = v
+        if k == 'X-Forwarded-Port':
+            environ['SERVER_PORT'] = v
+        environ['HTTP_' + k.upper().replace('-', '_')] = v
+
+    return environ
--- functions/tiler/main.py
+++ functions/tiler/main.py
@@ -1,70 +1,18 @@
 # coding=utf-8
 
-import base64
-import collections
 import os
-import sys
 
-from raven import Client
-from werkzeug.routing import NotFound
+import awsgi
 
-import app
 from app import app as tiler
 
 
-sentry = Client()
-
-
 def handle(event, context):
-    # TODO populate with request headers + query parameters and use tiler.full_dispatch_request
-    # (this forms the basis for a Lambda Flask adapter; perhaps a generic WSGI adapter would be
-    # better)
-    # see http://werkzeug.pocoo.org/docs/0.11/test/#environment-building
-    # see http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-create-api-as-simple-proxy-for-lambda.html#api-gateway-create-api-as-simple-proxy-for-lambda-test
-    with tiler.app_context(), tiler.test_request_context():
-        try:
-            (endpoint, args) = tiler.url_map.bind("", path_info=event["path"]).match()
-        except NotFound:
-            return {
-                "statusCode": 404,
-            }
-
-        try:
-            rsp = getattr(app, endpoint)(**args)
-
-            # could bypass this check by properly dispatching (and always receiving a Response)
-            if isinstance(rsp, collections.Iterable):
-                data = rsp[0] if 0 < len(rsp) else ""
-                status_code = rsp[1] if 1 < len(rsp) else 200
-                headers = rsp[2] if 2 < len(rsp) else {}
-            else:
-                if rsp.is_streamed:
-                    data = bytearray()
-                    [data.extend(x) for x in rsp.response]
-                else:
-                    data = rsp.get_data()
-                status_code = rsp.status_code
-                headers = dict(rsp.headers)
-
-            return {
-                "statusCode": status_code,
-                "headers": headers,
-                "body": base64.b64encode(data),
-                "isBase64Encoded": True,
-            }
-        except:
-            try:
-                rsp = tiler.handle_user_exception(sys.exc_info()[1])
-
-                return {
-                    "statusCode": rsp.status_code,
-                    "headers": dict(rsp.headers),
-                    "body": base64.b64encode(rsp.get_data()),
-                    "isBase64Encoded": True,
-                }
-            except:
-                sentry.captureException()
-                raise
+    # Cloudfront isn't configured to pass Host headers, so this is the API Gateway hostname
+    event['headers']['Host'] = os.environ['SERVER_NAME']
+    # (Our) Cloudfront doesn't pass X-Forwarded-Proto, so the value provided is from API Gateway
+    event['headers']['X-Forwarded-Proto'] = 'http'
+    return awsgi.response(tiler, event, context)
 
 
 if __name__ == '__main__':
