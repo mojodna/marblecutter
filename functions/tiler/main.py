f9c057b77a3be572256142e829ca5c0ba3407d0c
Seth Fitzsimmons <seth@mojodna.net>
2017-04-10 09:43:55 -0700

    Surface logs when running in Lambda mode

--- functions/tiler/main.py
+++ functions/tiler/main.py
@@ -1,29 +1,28 @@
+# noqa
 # coding=utf-8
 
+import logging
 import os
 
 import awsgi
-
 from app import app as tiler
 
 
-def handle(event, context):
-    # Cloudfront isn't configured to pass Host headers, so the provided Host header is the API Gateway hostname
+# reset the Lambda logger
+root = logging.getLogger()
+if root.handlers:
+    for handler in root.handlers:
+        root.removeHandler(handler)
+
+logging.basicConfig(level=logging.INFO)
+
+
+def handle(event, context): # noqa
+    # Cloudfront isn't configured to pass Host headers, so the provided Host
+    # header is the API Gateway hostname
     event['headers']['Host'] = os.environ['SERVER_NAME']
-    # Cloudfront drops X-Forwarded-Proto, so the value provided is from API Gateway
+    # Cloudfront drops X-Forwarded-Proto, so the value provided is from API
+    # Gateway
     event['headers']['X-Forwarded-Proto'] = 'http'
+
     return awsgi.response(tiler, event, context)
-
-
-if __name__ == '__main__':
-    print(handle({
-        'path': '/57fc935b84ae75bb00ec751b/wmts',
-    }, None))
-
-    print(handle({
-        'path': '/57fc935b84ae75bb00ec751b/12/1202/1833.png',
-    }, None))
-
-    print(handle({
-        'path': '/57fc935b84ae75bb00ec751b/12/1202/1833@2px.png',
-    }, None))
