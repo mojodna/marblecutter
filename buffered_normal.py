655c71d8c68bd15d7dd88f41493d6e1b82d12338
Seth Fitzsimmons <seth@mojodna.net>
2017-04-27 19:21:20 -0700

    Repeat edge pixels to fill collar

--- buffered_normal.py
+++ buffered_normal.py
@@ -5,6 +5,7 @@ from __future__ import division
 import logging
 from StringIO import StringIO
 
+import numpy as np
 from PIL import Image
 
 from normal import render_normal
@@ -20,9 +21,32 @@ NAME = 'Buffered Normal'
 
 
 def render(tile, (data, buffers)): # noqa
-    buffers = map(lambda x: x - COLLAR, buffers)
+    buffers = map(lambda x: max(0, x - COLLAR), buffers)
     data = data[0][buffers[3]:data.shape[1] - buffers[1],
                    buffers[0]:data.shape[2] - buffers[2]]
+
+    if buffers[0] == 0:
+        # empty left
+        cols = data[:, :COLLAR]
+        data = np.hstack((data, cols))
+        pass
+
+    if buffers[2] == 0:
+        # empty right
+        cols = data[:, -COLLAR:]
+        data = np.hstack((cols, data))
+        pass
+
+    if buffers[3] == 0:
+        # empty top buffer; repeat
+        rows = data[:COLLAR]
+        data = np.vstack((rows, data))
+        buffers[3] = COLLAR
+
+    if buffers[1] == 0:
+        # empty bottom buffer; repeat
+        data = np.vstack((data, rows))
+        buffers[1] = COLLAR
 
     imgarr = render_normal(tile, data, buffers)
 
