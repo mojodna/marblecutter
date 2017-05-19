4a30c9d77f938f4b303408ef46e1a360d0caf672
Seth Fitzsimmons <seth@mojodna.net>
2017-05-19 14:24:17 -0700

    Buffer footprints by sample rate + 20%
    When sampling, rio footprint produces footprints that may be inset.

--- bin/ingest_footprints.sh
+++ bin/ingest_footprints.sh
@@ -22,4 +22,6 @@ find . -type f -name \*_footprint.json | \
     filename=$(basename ${footprint%_footprint.json}.tif)
     echo "UPDATE footprints SET resolution=${resolution}, source='${source}', url='s3://${bucket}/${source}/0/${filename}', approximate_zoom=least(22, ceil(log(2.0, ((2 * pi() * 6378137) / (${resolution} * 256))::numeric))) WHERE filename='${filename}';"
     echo "UPDATE footprints SET min_zoom=approximate_zoom - 3, max_zoom=approximate_zoom WHERE filename='${filename}';"
+    # 120 is the number of pixels to buffer; it's the sample value for rio footprint + 20%
+    echo "UPDATE footprints SET wkb_geometry=ST_Multi(ST_Buffer(wkb_geometry::geography, resolution * 120)::geometry)"
   done | psql -q
