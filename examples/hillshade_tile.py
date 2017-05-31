c964b5b980597a4cadb1bb0c923ce0dc47b65b48
Seth Fitzsimmons <seth@mojodna.net>
2017-05-31 13:23:20 -0700

    Cleaner multi-format hillshade tile example

--- examples/hillshade_tile.py
+++ examples/hillshade_tile.py
@@ -13,18 +13,31 @@ logging.basicConfig(level=logging.INFO)
 
 
 if __name__ == "__main__":
-    tile = Tile(324, 787, 11)
+    tile = Tile(1308, 3164, 13)
+
     hillshade = Hillshade(resample=True, add_slopeshade=True)
-    (content_type, data) = tiling.render_tile(tile, format=GeoTIFF(), transformation=hillshade, scale=2)
+    (content_type, data) = tiling.render_tile(
+        tile, format=GeoTIFF(), transformation=hillshade, scale=1)
 
     print("Content-type: ", content_type)
 
-    with open("tmp/11_324_787_hillshade.tif", "w") as f:
+    with open("tmp/{}_{}_{}_hillshade.tif".format(
+                tile.z, tile.x, tile.y), "w") as f:
         f.write(data)
 
-    (content_type, data) = tiling.render_tile(tile, format=ColorRamp("png"), transformation=hillshade, scale=2)
+    (content_type, data) = tiling.render_tile(tile, format=GeoTIFF())
 
     print("Content-type: ", content_type)
 
-    with open("tmp/11_324_787_hillshade.png", "w") as f:
+    with open("tmp/{}_{}_{}.tif".format(tile.z, tile.x, tile.y), "w") as f:
+        f.write(data)
+
+    # tile = Tile(654, 1582, 12)
+    (content_type, data) = tiling.render_tile(
+        tile, format=ColorRamp("png"), transformation=hillshade, scale=2)
+
+    print("Content-type: ", content_type)
+
+    with open("tmp/{}_{}_{}_hillshade.png".format(
+                tile.z, tile.x, tile.y), "w") as f:
         f.write(data)
