fe9a305354a463a7e76fb5dedad5e7e0eb3036a3
Seth Fitzsimmons <seth@mojodna.net>
2017-06-05 14:44:29 -0700

    Bulk Skadi tiles

---
+++ examples/render_bulk_skadi.py
@@ -0,0 +1,45 @@
+# noqa
+# coding=utf-8
+from __future__ import print_function
+
+import logging
+import multiprocessing
+from multiprocessing.dummy import Pool
+
+from marblecutter import skadi
+
+logging.basicConfig(level=logging.INFO)
+
+POOL = Pool(multiprocessing.cpu_count() * 4)
+
+
+def render_tile(tile):
+    (content_type, data) = skadi.render_tile(tile)
+
+    print("Content-type: ", content_type)
+
+    with open("tmp/{}.hgt.gz".format(tile), "w") as f:
+        f.write(data)
+
+
+def queue_tile(tile):
+    print(tile)
+    POOL.apply_async(
+        render_tile,
+        args=[tile])
+
+
+if __name__ == "__main__":
+    # NOTE this will generate some invalid tile names. GDAL's SRTMHGT will
+    # prevent them from actually being created thought
+    for ns in ("N", "S"):
+        for lat in range(1):
+        # for lat in range(90):
+            for ew in ("E", "W"):
+                for lon in range(1):
+                # for lon in range(180):
+                    tile = "{}{:02d}{}{:03d}".format(ns, lat, ew, lon)
+                    queue_tile(tile)
+
+    POOL.close()
+    POOL.join()
