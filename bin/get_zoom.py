74a0630e66af5f14be9c39692b00866ee2f43bab
Seth Fitzsimmons <seth@mojodna.net>
2017-07-19 15:15:49 -0700

    yapf, isort

--- bin/get_zoom.py
+++ bin/get_zoom.py
@@ -5,17 +5,17 @@ from __future__ import print_function
 
 import math
 import os
-import re
 import sys
 
-from haversine import haversine
 import rasterio
-from rasterio.warp import transform_bounds
+from haversine import haversine
 
 
 def get_zoom_offset(width, height, approximate_zoom):
-    return len([x for x in range(approximate_zoom)
-                if (height / (2 ** (x + 1))) >= 1 and (width / (2 ** (x + 1))) >= 1])
+    return len([
+        x for x in range(approximate_zoom)
+        if (height / (2**(x + 1))) >= 1 and (width / (2**(x + 1))) >= 1
+    ])
 
 
 def get_resolution(input):
@@ -28,18 +28,26 @@ def get_resolution(input):
                 top = ((src.bounds[0] + src.bounds[2]) / 2, src.bounds[3])
                 bottom = ((src.bounds[0] + src.bounds[2]) / 2, src.bounds[1])
 
-                return max(haversine(left, right) * 1000 / src.width, haversine(top, bottom) * 1000 / src.height)
-            return max((src.bounds.right - src.bounds.left) / src.width, (src.bounds.top - src.bounds.bottom) / src.height)
+                return max(
+                    haversine(left, right) * 1000 / src.width,
+                    haversine(top, bottom) * 1000 / src.height)
+            return max((src.bounds.right - src.bounds.left) / src.width,
+                       (src.bounds.top - src.bounds.bottom) / src.height)
 
 
 def get_zoom(input):
-    return min(22, int(math.ceil(math.log((2 * math.pi * 6378137) /
-                                          (get_resolution(input) * 256)) / math.log(2))))
+    return min(22,
+               int(
+                   math.ceil(
+                       math.log((2 * math.pi * 6378137) / (
+                           get_resolution(input) * 256)) / math.log(2))))
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("usage: {} <input>".format(os.path.basename(sys.argv[0])), file=sys.stderr)
+        print(
+            "usage: {} <input>".format(os.path.basename(sys.argv[0])),
+            file=sys.stderr)
         exit(1)
 
     input = sys.argv[1]
