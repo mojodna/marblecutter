0bfd8b71da8f5c08bcedfc9f9a6f465fd2eae9a1
Seth Fitzsimmons <seth@mojodna.net>
2017-08-22 15:47:50 -0700

    More package metadata

--- setup.py
+++ setup.py
@@ -4,4 +4,12 @@ version = '0.0.0'
 
 setup(
     name='marblecutter',
+    version=version,
+    description='Raster manipulation',
+    url='https://github.com/mojodna/marblecutter',
+    author='Seth Fitzsimmons',
+    author_email='seth@mojodna.net',
+    license='BSD',
+    packages=['marblecutter'],
+    zip_safe=False,
 )
