c101808a1dda167887c5ae0ab902476a9803d927
Ian Dees <ian.dees@gmail.com>
2017-08-29 16:00:51 -0500

    Skadi Batch jobs support

--- examples/render_bulk_skadi.py
+++ examples/render_bulk_skadi.py
@@ -1,43 +1,144 @@
-# noqa
 # coding=utf-8
 from __future__ import print_function
 
+import argparse
+import boto3
+import botocore
 import logging
 import multiprocessing
+import os
+import time
+from functools import wraps
 from multiprocessing.dummy import Pool
 
 from marblecutter import skadi
+from marblecutter.sources import PostGISAdapter
+from marblecutter.stats import Timer
 
 logging.basicConfig(level=logging.INFO)
+# Quieting boto messages down a little
+logging.getLogger('boto3.resources.action').setLevel(logging.WARNING)
+logging.getLogger('botocore').setLevel(logging.WARNING)
+logging.getLogger('marblecutter.mosaic').setLevel(logging.WARNING)
+logger = logging.getLogger('batchtiler')
 
 POOL = Pool(multiprocessing.cpu_count() * 4)
+S3_BUCKET = os.environ.get('S3_BUCKET')
+CATALOG = PostGISAdapter()
 
 
-def render_tile(tile):
-    (headers, data) = skadi.render_tile(tile)
+# From https://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+def retry(ExceptionToCheck,
+          tries=4,
+          delay=3,
+          backoff=2,
+          max_delay=60,
+          logger=None):
+    def deco_retry(f):
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except ExceptionToCheck, e:
+                    msg = "%s, Retrying in %d seconds..." % (str(e), mdelay)
+                    if logger:
+                        logger.exception(msg)
+                    else:
+                        print(msg)
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+                    mdelay = min(mdelay, max_delay)
+            return f(*args, **kwargs)
 
-    print("Headers: ", headers)
+        return f_retry  # true decorator
 
-    with open("tmp/{}.hgt.gz".format(tile), "w") as f:
-        f.write(data)
+    return deco_retry
 
 
-def queue_tile(tile):
-    print(tile)
-    POOL.apply_async(render_tile, args=[tile])
+@retry((Exception, ), tries=30, logger=logger)
+def write_to_s3(bucket, key,
+                data, headers, overwrite=False):
+
+    content_type = headers.pop('Content-Type')
+
+    obj = bucket.Object(key)
+    if overwrite:
+        obj.put(
+            Body=data,
+            ContentType=content_type,
+            Metadata={k: headers[k]
+                      for k in headers})
+    else:
+        try:
+            obj.load()
+        except botocore.exceptions.ClientError as e:
+            # If a client error is thrown, then check that it was a 404 error.
+            # If it was a 404 error, then the object does not exist.
+            error_code = int(e.response['Error']['Code'])
+            if error_code == 404:
+                obj.put(
+                    Body=data,
+                    ContentType=content_type,
+                    Metadata={
+                        k: headers[k]
+                        for k in headers
+                    })
+
+    return obj
+
+
+def render_tile_and_put_to_s3(tile, s3_bucket, key_prefix):
+    with Timer() as t:
+        (headers, data) = skadi.render_tile(tile, CATALOG)
+    logger.info("Skadi tile %s rendered in %0.3f",
+                tile, t.elapsed)
+
+    with Timer() as t:
+        # Each thread needs its own boto3 Session object – it's not threadsafe
+        session = boto3.session.Session()
+        s3 = session.resource('s3')
+        bucket = s3.Bucket(s3_bucket)
+        key = "skadi/{}/{}.hgt.gz".format(
+            tile[:3],
+            tile,
+        )
+
+        if key_prefix:
+            key = key_prefix + '/' + key
+
+        obj = write_to_s3(bucket, key, data, headers, overwrite=True)
+    logger.info("Skadi tile %s uploaded to s3://%s/%s in %0.3f",
+                tile, obj.bucket_name, obj.key, t.elapsed)
+
+
+def render_tile_exc_wrapper(tile, s3_bucket, key_prefix):
+    try:
+        render_tile_and_put_to_s3(tile, s3_bucket, key_prefix)
+    except Exception:
+        logger.exception('Error while processing tile %s', tile)
+        raise
+
+
+def queue_tile(tile, bucket, key_prefix):
+    POOL.apply_async(render_tile_exc_wrapper, args=[tile, bucket, key_prefix])
 
 
 if __name__ == "__main__":
-    # NOTE this will generate some invalid tile names. GDAL's SRTMHGT will
-    # prevent them from actually being created thought
-    for ns in ("N", "S"):
-        for lat in range(1):
-            # for lat in range(90):
-            for ew in ("E", "W"):
-                for lon in range(1):
-                    # for lon in range(180):
-                    tile = "{}{:02d}{}{:03d}".format(ns, lat, ew, lon)
-                    queue_tile(tile)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('bucket')
+    parser.add_argument('tiles', nargs='+')
+    parser.add_argument('--prefix')
+    args = parser.parse_args()
+
+    logger.info("Writing %s Skadi tiles to S3 bucket %s with prefix '%s'",
+                len(args.tiles), args.bucket, args.prefix)
+
+    for tile in args.tiles:
+        queue_tile(tile, args.bucket, args.prefix)
 
     POOL.close()
     POOL.join()
+    logger.info("Done.")
---
+++ examples/submit_skadi_tile_jobs.py
@@ -0,0 +1,65 @@
+import argparse
+import boto3
+import os
+from itertools import islice
+
+
+def grouper(iterable, n):
+    """Yield n-length chunks of the iterable"""
+    it = iter(iterable)
+    while True:
+        chunk = tuple(islice(it, n))
+        if not chunk:
+            return
+        yield chunk
+
+
+def generate_tiles():
+    # NOTE this will generate some invalid tile names. GDAL's SRTMHGT will
+    # prevent them from actually being created thought
+    for ns in ("N", "S"):
+        # for lat in range(1):
+        for lat in range(90):
+            for ew in ("E", "W"):
+                # for lon in range(1):
+                for lon in range(180):
+                    tile = "{}{:02d}{}{:03d}".format(ns, lat, ew, lon)
+                    yield tile
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('bucket')
+    parser.add_argument('per_job', type=int)
+    parser.add_argument('--prefix')
+    args = parser.parse_args()
+
+    client = boto3.client('batch')
+
+    database_url = os.environ.get('DATABASE_URL')
+    assert database_url, "Please set a DATABASE_URL environment variable"
+
+    for tile_group in grouper(generate_tiles(), args.per_job):
+        command_list = [
+            'python', 'examples/render_bulk_skadi.py', args.bucket
+        ]
+
+        if args.prefix:
+            command_list.append('--prefix')
+            command_list.append(args.prefix)
+
+        command_list.extend(tile_group)
+
+        result = client.submit_job(
+            jobName='skadi-' + tile_group[0],
+            jobQueue='tiling-skadi-20170828',
+            overrides={
+                'command': command_list,
+                'environment': [
+                    {'name': 'DATABASE_URL',
+                     'value': database_url}
+                ]
+            }
+        )
+
+        print "name: {jobName}, id: {jobId}".format(result)
