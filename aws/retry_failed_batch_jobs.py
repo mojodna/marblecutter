617d0862c1023ac49800b69ea3520f952084fdd8
Ian Dees <ian.dees@gmail.com>
2017-11-02 07:39:15 -0500

    Improve failed job retry script

--- aws/retry_failed_batch_jobs.py
+++ aws/retry_failed_batch_jobs.py
@@ -1,6 +1,7 @@
 import argparse
 import boto3
 import time
+from itertools import islice
 
 
 def iter_jobs(client, queue_arn, status):
@@ -19,52 +20,61 @@ def iter_jobs(client, queue_arn, status):
             break
 
 
+def grouper(n, iterable):
+    iterable = iter(iterable)
+    return iter(lambda: list(islice(iterable, n)), [])
+
+
 def retry_jobs(queue_arn, wait=0, since=0):
     client = boto3.client('batch')
     oldest_timestamp = None
+    job_count = 0
 
-    for job_summary in iter_jobs(client, queue_arn, 'FAILED'):
-        job_id = job_summary['jobId']
+    for summary_group in grouper(50, iter_jobs(client, queue_arn, 'FAILED')):
+        job_ids = [summary['jobId'] for summary in filter(None, summary_group)]
 
-        job_data = client.describe_jobs(jobs=[job_id])['jobs'][0]
+        job_datas = client.describe_jobs(jobs=job_ids)['jobs']
 
-        stopped_at = job_data['stoppedAt']
-        if oldest_timestamp is None or stopped_at > oldest_timestamp:
-            oldest_timestamp = stopped_at
+        for job_data in job_datas:
+            stopped_at = job_data['stoppedAt']
+            if oldest_timestamp is None or stopped_at > oldest_timestamp:
+                oldest_timestamp = stopped_at
+            job_count += 1
 
-        if job_data['stoppedAt'] <= since:
-            print "Skipping job {} ({}) that stopped at {} because it's too old".format(
+            if job_data['stoppedAt'] <= since:
+                print "Skipping job {} ({}) that stopped at {} because it's too old".format(
+                    job_data['jobName'],
+                    job_data['jobId'],
+                    job_data['stoppedAt'],
+                )
+                continue
+
+            cloned_job = {
+                'jobName': job_data['jobName'],
+                'jobQueue': job_data['jobQueue'],
+                'jobDefinition': job_data['jobDefinition'],
+                'containerOverrides': {
+                    'vcpus': job_data['container']['vcpus'],
+                    'memory': job_data['container']['memory'],
+                    'command': job_data['container']['command'],
+                    'environment': job_data['container']['environment'],
+                },
+                'retryStrategy': job_data['retryStrategy'],
+            }
+
+            submitted_job = client.submit_job(**cloned_job)
+
+            print "Re-submitted job {} ({}) that stopped at {} and got jobId {}".format(
                 job_data['jobName'],
                 job_data['jobId'],
                 job_data['stoppedAt'],
+                submitted_job['jobId']
             )
-            continue
 
-        cloned_job = {
-            'jobName': job_data['jobName'],
-            'jobQueue': job_data['jobQueue'],
-            'jobDefinition': job_data['jobDefinition'],
-            'containerOverrides': {
-                'vcpus': job_data['container']['vcpus'],
-                'memory': job_data['container']['memory'],
-                'command': job_data['container']['command'],
-                'environment': job_data['container']['environment'],
-            },
-            'retryStrategy': job_data['retryStrategy'],
-        }
+            time.sleep(wait / 1000.0)
 
-        submitted_job = client.submit_job(**cloned_job)
-
-        print "Re-submitted job {} ({}) that stopped at {} and got jobId {}".format(
-            job_data['jobName'],
-            job_data['jobId'],
-            job_data['stoppedAt'],
-            submitted_job['jobId']
-        )
-
-        time.sleep(wait / 1000.0)
-
-    print "Oldest job seen stopped at {}".format(
+    print "Saw {} jobs in FAILED queue. Oldest job stopped at {}".format(
+        job_count,
         oldest_timestamp
     )
 
