0c5492c7f83fe22de879949ef9a8345b030b1e68
Seth Fitzsimmons <seth@mojodna.net>
2017-12-02 12:28:03 -0800

    Extract recipe handling

--- marblecutter/mosaic.py
+++ marblecutter/mosaic.py
@@ -9,19 +9,11 @@ from concurrent import futures
 import numpy as np
 
 from rasterio import warp
-from rio_tiler import utils
-from rio_toa import reflectance
 
-from .utils import Bounds, PixelCollection, Source
+from . import recipes
+from .utils import Bounds, PixelCollection
 
 LOG = logging.getLogger(__name__)
-
-
-def mask_outliers(data, m=2.):
-    d = np.abs(data - np.median(data.compressed()))
-    mdev = np.median(d.compressed())
-    s = d / mdev if mdev else 0.
-    return np.where(s < m, False, True)
 
 
 def composite(sources, bounds, dims, target_crs, band_count):
@@ -40,27 +32,7 @@ def composite(sources, bounds, dims, target_crs, band_count):
     canvas.mask = True
     canvas_bounds = Bounds((left, bottom, right, top), target_crs)
 
-    # TODO preprocess by recipe
-    actual_sources = []
-    band_mapping = {
-        "r": 0,
-        "g": 1,
-        "b": 2,
-    }
-    for source in sources:
-        if "landsat8" in source.recipes:
-            for target_band, source_band in source.band_info.iteritems():
-                band = band_mapping.get(target_band)
-                if band is not None:
-                    s = Source(
-                        source.url.replace("{band}", str(source_band)),
-                        source.name, source.resolution, source.band_info,
-                        source.meta, source.recipes, band)
-                    actual_sources.append(s)
-        else:
-            actual_sources.append(source)
-
-    sources = actual_sources
+    sources = recipes.preprocess(sources)
 
     def _read_window(source):
         with get_source(source.url) as src:
@@ -76,66 +48,8 @@ def composite(sources, bounds, dims, target_crs, band_count):
             if not window_data:
                 return
 
-            data = window_data.data
-
-            # TODO extract recipe implementations
-
-            if "mask_outliers" in source.recipes:
-                # mask outliers (intended for DEM boundaries)
-                LOG.info("masking outliers")
-                data.mask[0] = np.logical_or(data.mask[0],
-                                             mask_outliers(data[0], 100.))
-
-            if "landsat8" in source.recipes:
-                LOG.info("Applying landsat 8 recipe")
-                source_band = source.url.split("/")[-1].split("_B")[1][0]
-
-                sun_elev = source.meta["L1_METADATA_FILE"]["IMAGE_ATTRIBUTES"][
-                    "SUN_ELEVATION"]
-                multi_reflect = source.meta[
-                    "L1_METADATA_FILE"]["RADIOMETRIC_RESCALING"].get(
-                        "REFLECTANCE_MULT_BAND_{}".format(source_band))
-                add_reflect = source.meta[
-                    "L1_METADATA_FILE"]["RADIOMETRIC_RESCALING"].get(
-                        "REFLECTANCE_ADD_BAND_{}".format(source_band))
-
-                data = 10000 * reflectance.reflectance(
-                    data, multi_reflect, add_reflect, sun_elev, src_nodata=0)
-
-                # calculate local min/max as fallbacks
-                default_min = 0
-                default_max = 65535
-
-                min_val = source.meta.get("values",
-                                          {}).get(source_band, {}).get(
-                                              "min", default_min)
-                max_val = source.meta.get("values",
-                                          {}).get(source_band, {}).get(
-                                              "max", default_max)
-
-                if (min_val == default_min and max_val == default_max
-                        and len(data.compressed()) > 0):
-                    local_min, local_max = np.percentile(
-                        data.compressed(), (2, 98))
-                    min_val = max(min_val, local_min)
-                    max_val = min(max_val, local_max)
-
-                data = np.ma.where(data > 0,
-                                   utils.linear_rescale(
-                                       data,
-                                       in_range=[min_val, max_val],
-                                       out_range=[0, 1]), 0)
-
-                window_data = PixelCollection(data, window_data.bounds,
-                                              source.band)
-
-            if "imagery" in source.recipes:
-                LOG.info("Applying imagery recipe")
-                # normalize to 0..1 based on the range of the source type (only
-                # for int*s)
-                if not np.issubdtype(src.meta["dtype"], float):
-                    data /= np.iinfo(src.meta["dtype"]).max
-                    window_data = PixelCollection(data, window_data.bounds)
+            window_data = recipes.apply(
+                source.recipes, window_data, source=source, ds=src)
 
             return source, window_data
 
---
+++ marblecutter/recipes.py
@@ -0,0 +1,97 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function
+
+import logging
+
+import numpy as np
+
+from rio_tiler import utils
+from rio_toa import reflectance
+
+from .utils import PixelCollection, Source
+
+BAND_MAPPING = {
+    "r": 0,
+    "g": 1,
+    "b": 2,
+}
+LOG = logging.getLogger(__name__)
+
+
+def apply(recipes, pixels, source=None, ds=None):
+    data = pixels.data
+
+    if "mask_outliers" in recipes:
+        # mask outliers (intended for DEM boundaries)
+        LOG.info("masking outliers")
+        data.mask[0] = np.logical_or(data.mask[0], mask_outliers(
+            data[0], 100.))
+
+    if "landsat8" in recipes:
+        LOG.info("Applying landsat 8 recipe")
+        source_band = source.url.split("/")[-1].split("_B")[1][0]
+
+        sun_elev = source.meta["L1_METADATA_FILE"]["IMAGE_ATTRIBUTES"][
+            "SUN_ELEVATION"]
+        multi_reflect = source.meta[
+            "L1_METADATA_FILE"]["RADIOMETRIC_RESCALING"].get(
+                "REFLECTANCE_MULT_BAND_{}".format(source_band))
+        add_reflect = source.meta[
+            "L1_METADATA_FILE"]["RADIOMETRIC_RESCALING"].get(
+                "REFLECTANCE_ADD_BAND_{}".format(source_band))
+
+        data = 10000 * reflectance.reflectance(
+            data, multi_reflect, add_reflect, sun_elev, src_nodata=0)
+
+        # calculate local min/max as fallbacks
+        default_min = 0
+        default_max = 65535
+
+        min_val = source.meta.get("values", {}).get(source_band, {}).get(
+            "min", default_min)
+        max_val = source.meta.get("values", {}).get(source_band, {}).get(
+            "max", default_max)
+
+        if (min_val == default_min and max_val == default_max
+                and len(data.compressed()) > 0):
+            local_min, local_max = np.percentile(data.compressed(), (2, 98))
+            min_val = max(min_val, local_min)
+            max_val = min(max_val, local_max)
+
+        data = np.ma.where(data > 0,
+                           utils.linear_rescale(
+                               data,
+                               in_range=[min_val, max_val],
+                               out_range=[0, 1]), 0)
+
+    if "imagery" in recipes:
+        LOG.info("Applying imagery recipe")
+        # normalize to 0..1 based on the range of the source type (only
+        # for int*s)
+        if not np.issubdtype(ds.meta["dtype"], float):
+            data /= np.iinfo(ds.meta["dtype"]).max
+
+    # TODO source.band should be pixels.band, which requires read_window to be
+    # band-aware
+    return PixelCollection(data, pixels.bounds, source.band)
+
+
+def mask_outliers(data, m=2.):
+    d = np.abs(data - np.median(data.compressed()))
+    mdev = np.median(d.compressed())
+    s = d / mdev if mdev else 0.
+    return np.where(s < m, False, True)
+
+
+def preprocess(sources):
+    for source in sources:
+        if "landsat8" in source.recipes:
+            for target_band, source_band in source.band_info.iteritems():
+                band = BAND_MAPPING.get(target_band)
+                if band is not None:
+                    yield Source(
+                        source.url.replace("{band}", str(source_band)),
+                        source.name, source.resolution, source.band_info,
+                        source.meta, source.recipes, band)
+        else:
+            yield source
