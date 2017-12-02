e27dfc56b603dd5274d0a6141b239d5ec8c8a6b1
Seth Fitzsimmons <seth@mojodna.net>
2017-12-02 11:06:56 -0800

    Cleanup

--- marblecutter/mosaic.py
+++ marblecutter/mosaic.py
@@ -3,8 +3,8 @@
 from __future__ import absolute_import, division, print_function
 
 import logging
-from concurrent import futures
 import multiprocessing
+from concurrent import futures
 
 import numpy as np
 
