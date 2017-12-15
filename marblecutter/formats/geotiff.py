4b1b4afcbcbb05758b6001e8b3ffd38eb23aa166
Seth Fitzsimmons <seth@mojodna.net>
2017-12-15 10:07:10 -0800

    Allow blocksize to be configurable
    Mapzen Terrain Tiles require a block size of 256 to be
    backward-compatible with v1. Otherwise, a consensus around a block size
    of 512 has emerged.

--- marblecutter/formats/geotiff.py
+++ marblecutter/formats/geotiff.py
@@ -11,7 +11,7 @@ from .. import _nodata, get_resolution_in_meters
 CONTENT_TYPE = "image/tiff"
 
 
-def GeoTIFF(area_or_point="Area"):
+def GeoTIFF(area_or_point="Area", blocksize=512):
     def _format(pixels, data_format):
         data, (data_bounds, data_crs) = pixels
         if data_format is not "raw":
@@ -35,8 +35,8 @@ def GeoTIFF(area_or_point="Area"):
             predictor = 2
 
         meta = {
-            "blockxsize": 512 if width >= 512 else width,
-            "blockysize": 512 if height >= 512 else height,
+            "blockxsize": blocksize if width >= blocksize else width,
+            "blockysize": blocksize if height >= blocksize else height,
             "compress": "deflate",
             "count": count,
             "crs": data_crs,
@@ -46,7 +46,7 @@ def GeoTIFF(area_or_point="Area"):
             "predictor": predictor,
             "height": height,
             "width": width,
-            "tiled": width >= 512 and height >= 512,
+            "tiled": width >= blocksize and height >= blocksize,
             "transform": transform.from_bounds(
                 *data_bounds,
                 width=width,
