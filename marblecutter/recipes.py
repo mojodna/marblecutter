213ffa3aa4bb725bd55750df85bdc735a3c43bce
Seth Fitzsimmons <seth@mojodna.net>
2018-04-26 18:07:09 -0700

    Global and per-band linear stretching
    Global stretching uses min/max values across all bands, which may
    produce noisy output under some circumstances. Per-band stretching uses
    min/max values from each band, effectively increasing contrast.

--- marblecutter/recipes.py
+++ marblecutter/recipes.py
@@ -82,38 +82,57 @@ def apply(recipes, pixels, source=None):
             # TODO use this as a mask instead
             data = data[0:3]
 
-        # rescale after reducing and before increasing dimensionality
-        if data.dtype != np.uint8 and not np.issubdtype(data.dtype, float):
-            # rescale non-8-bit sources (assuming that they're raw sensor data)
-
-            for band in xrange(0, data.shape[0]):
-                min_val = source.meta.get("values", {}).get(band, {}).get(
-                    "min", dtype_min
+        if "linear_stretch" in recipes:
+            if recipes["linear_stretch"] == "global":
+                data = utils.linear_rescale(
+                    data,
+                    in_range=(np.min(data), np.max(data)),
+                    out_range=(dtype_min, dtype_max),
                 )
-                max_val = source.meta.get("values", {}).get(band, {}).get(
-                    "max", dtype_max
-                )
-
-                if (
-                    min_val == dtype_min
-                    and max_val == dtype_max
-                    and len(data.compressed()) > 0
-                ):
-                    local_min, local_max = np.percentile(
-                        data[band].compressed(), (2, 98)
+            elif recipes["linear_stretch"] == "per_band":
+                for band in xrange(0, data.shape[0]):
+                    data[band] = np.ma.where(
+                        data[band] > 0,
+                        utils.linear_rescale(
+                            data[band],
+                            in_range=(np.min(data[band]), np.max(data[band])),
+                            out_range=(dtype_min, dtype_max),
+                        ),
+                        0,
                     )
-                    min_val = max(min_val, local_min)
-                    max_val = min(max_val, local_max)
+        else:
+            # rescale after reducing and before increasing dimensionality
+            if data.dtype != np.uint8 and not np.issubdtype(data.dtype, float):
+                # rescale non-8-bit sources (assuming that they're raw sensor data)
 
-                data[band] = np.ma.where(
-                    data[band] > 0,
-                    utils.linear_rescale(
-                        data[band],
-                        in_range=[min_val, max_val],
-                        out_range=[dtype_min, dtype_max],
-                    ),
-                    0,
-                )
+                for band in xrange(0, data.shape[0]):
+                    min_val = source.meta.get("values", {}).get(band, {}).get(
+                        "min", dtype_min
+                    )
+                    max_val = source.meta.get("values", {}).get(band, {}).get(
+                        "max", dtype_max
+                    )
+
+                    if (
+                        min_val == dtype_min
+                        and max_val == dtype_max
+                        and len(data.compressed()) > 0
+                    ):
+                        local_min, local_max = np.percentile(
+                            data[band].compressed(), (2, 98)
+                        )
+                        min_val = max(min_val, local_min)
+                        max_val = min(max_val, local_max)
+
+                    data[band] = np.ma.where(
+                        data[band] > 0,
+                        utils.linear_rescale(
+                            data[band],
+                            in_range=(min_val, max_val),
+                            out_range=(dtype_min, dtype_max),
+                        ),
+                        0,
+                    )
 
         if data.shape[0] == 1:
             # likely greyscale image; use the same band on all channels
