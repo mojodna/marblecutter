5c9ea423e20789e34c2e038055c96f8682dd17da
Seth Fitzsimmons <seth@mojodna.net>
2018-05-03 10:22:39 -0700

    Optimal format uses PNGs or JPEGs as appropriate

---
+++ marblecutter/formats/jpeg.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+from io import BytesIO
+
+from PIL import Image
+
+from .. import _isimage
+
+CONTENT_TYPE = "image/jpeg"
+
+
+def JPEG():
+
+    def _format(pixels, data_format):
+        if not _isimage(data_format):
+            raise Exception("Must be an image format")
+
+        out = BytesIO()
+        im = Image.fromarray(pixels.data, data_format.upper())
+        im.save(out, "jpeg")
+
+        return (CONTENT_TYPE, out.getvalue())
+
+    return _format
---
+++ marblecutter/formats/optimal.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import logging
+
+import numpy as np
+
+from .. import _isimage
+from ..utils import PixelCollection
+from .jpeg import JPEG
+from .png import PNG
+
+LOG = logging.getLogger(__name__)
+JPEG_FORMAT = JPEG()
+PNG_FORMAT = PNG()
+
+
+def OPTIMAL():
+
+    def _format(pixels, data_format):
+        if not _isimage(data_format):
+            raise Exception("Must be an image format")
+
+        alpha = pixels.data[:, :, 3]
+
+        if np.equal(alpha, 255).all():
+            # solid
+            rgb_pixels = PixelCollection(
+                pixels.data[:, :, 0:3], pixels.bounds, pixels.band
+            )
+            return JPEG_FORMAT(rgb_pixels, "RGB")
+        else:
+            # partially transparent
+            return PNG_FORMAT(pixels, data_format)
+
+    return _format
