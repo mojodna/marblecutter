6bd0dd2c38e817d59875213eb71b9b6e5e1f7515
Seth Fitzsimmons <seth@mojodna.net>
2018-11-07 17:36:02 -0800

    Export Transformation

--- marblecutter/transformations/__init__.py
+++ marblecutter/transformations/__init__.py
@@ -1,9 +1,10 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-from . import buffer, colormap, greyscale, image
+from . import buffer, colormap, greyscale, image, utils
 
 Buffer = buffer.Buffer
 Colormap = colormap.Colormap
 Greyscale = greyscale.Greyscale
 Image = image.Image
+Transformation = utils.Transformation
