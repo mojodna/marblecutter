2a47bb2ca96280c7528e11dbd068125fd2660e38
Seth Fitzsimmons <seth@mojodna.net>
2018-12-17 15:49:59 -0800

    Always add an alpha channel
    This allows areas outside image bounds to be masked when dynamically
    reprojected using WarpedVRTs (otherwise they require nodata to be
    specified and are 0 by default without an indicator).

--- marblecutter/__init__.py
+++ marblecutter/__init__.py
@@ -237,7 +237,6 @@ def read_window(src, bounds, target_shape, source):
         resampling = Resampling[source.recipes.get("resample", "bilinear")]
 
     src_nodata = source.recipes.get("nodata", source.meta.get("nodata", src.nodata))
-    add_alpha = False
 
     if (
         any([MaskFlags.per_dataset in flags for flags in src.mask_flag_enums])
@@ -245,7 +244,6 @@ def read_window(src, bounds, target_shape, source):
     ):
         # prefer the mask if available
         src_nodata = None
-        add_alpha = True
 
     w, s, e, n = bounds.bounds
     vrt_transform = (
@@ -264,7 +262,7 @@ def read_window(src, bounds, target_shape, source):
         height=vrt_height,
         transform=vrt_transform,
         resampling=resampling,
-        add_alpha=add_alpha,
+        add_alpha=True,
     ) as vrt:
         dst_window = vrt.window(*bounds.bounds)
 
